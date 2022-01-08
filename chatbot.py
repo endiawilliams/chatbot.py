bf496b2e736301a496b74468f2a46d96de2f2454
endiawilliams <endiawilliams1@gmail.com>
2022-01-07 19:17:14 -0600

    Bot can now identify the first noun or personal pronoun of a sentence.

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,33 @@
 import nltk
+from nltk.tokenize import word_tokenize
+
+# noun, plural noun, proper noun, plural proper noun
+noun_codes = ["NN", "NNS", "NNP", "NNPS"]
+
+def id_pos(user_response):
+    output = nltk.pos_tag(word_tokenize(user_response))
+
+    noun_exists = False
+
+    for (word, pos) in output:
+        if pos in noun_codes:
+            noun_exists = True
+            print(word + " is the first noun of the sentence.")
+            break
+        if pos == "PRP":
+            noun_exists = True
+            print(word + " is the first personal pronoun of the sentence.")
+            break
+
+    if noun_exists == False:
+        print("No nouns or personal pronouns were found.")
 
 print("Hi, I'm chatbot.py! Nice to meet you.")
 
 user_response = input()
 
 while user_response != "Goodbye.":
-    print("Tell me more.")
+    id_pos(user_response)
     user_response = input()
 
 print("Bye.")
